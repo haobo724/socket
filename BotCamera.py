f46208ac8a00c2e572b493ede6cb2b274aa64f0d
haobo724 <s724724@gmail.com>
2022-06-17 17:51:34 +0200

    tof

--- BotCamera.py
+++ BotCamera.py
@@ -22,30 +22,7 @@ camera_bot.set(cv2.CAP_PROP_EXPOSURE,-7)
 print(camera_bot.get(cv2.CAP_PROP_EXPOSURE))
 print(camera_bot.get(cv2.CAP_PROP_BRIGHTNESS))
 
-if os.path.exists(template_dir):
-    for i in range(10):
-        img_file_path = os.path.join(template_dir, str(i) + '.jpg')
-        if not os.access(img_file_path, os.F_OK):
-            img_file_path = os.path.join(template_dir, 'result_' + str(i) + '.tiff')
 
-        t = cv2.imread(img_file_path, 0)
-        t = cv2.resize(t, TEMPLATE_size)
-        img_template.append(t)
-        print(f'[INFO] TEMPLATE {i} lOADED')
-else:
-    raise FileNotFoundError('NO TEMPLATE')
-
-
-def get_match_score(img, template):
-    # print(np.max(template))
-    tp = (img == 255) == (template == 255)
-    tn = (img == 0) == (template == 0)
-    fp = (img == 255) == (template == 0)
-    fn = (img == 0) == (template == 255)
-
-    # score =( np.sum(tp) + np.sum(tn)) / (np.sum(tp) + np.sum(tn) + np.sum(fp) + np.sum(fn))
-    score = (np.sum(tp) + np.sum(tn) - np.sum(fp) - np.sum(fn))
-    return score
 def OCR_THIRD(img):
     result = ocr.ocr(img, cls=False,det=False)
     for line in result:
@@ -59,82 +36,6 @@ def OCR_THIRD(img):
         result = -1
 
     return result
-
-@timer
-def OCR(imfrag):
-    '''
-    :return FORCE , HEIGHT
-    '''
-    _, imfrag_h = imfrag.shape
-
-    ret2, imfrag = cv2.threshold(imfrag, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    roi_size = TEMPLATE_size
-    # detect single digit and detect
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 定义矩形结构元素
-
-    # convert gray value for contour detection
-    cnts, _ = cv2.findContours(imfrag.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    digitCnts = []
-    xloc = np.array([])
-    for c in cnts:
-        (x, y, w, h) = cv2.boundingRect(c)
-        # print(h)
-        # if height is more than 50, then digit is detected
-        if h > 30 / 85 * imfrag_h:
-            digitCnts.append(c)
-            xloc = np.append(xloc, x)
-
-    # if no connected component is detected, return ''
-    if digitCnts == []:
-        print('no co')
-        return -1, -1
-    # sort using x direction
-    idx = np.argsort(xloc)
-    tmp = digitCnts.copy()
-    digitCnts = []
-    for i in idx:
-        digitCnts.append(tmp[i])
-
-    digit = ''
-    if len(digitCnts) > 3 or len(digitCnts) < 1:
-        print('detect error,Suggested click restart btn')
-        return -1, -1
-    # print(len(digitCnts))
-    for c in digitCnts:
-        (x, y, w, h) = cv2.boundingRect(c)
-        roi = imfrag[y:y + h, x:x + w]
-
-        if roi is not None:
-            roi = cv2.resize(roi, roi_size)
-            # cv2.imshow('roi',roi)
-            # cv2.waitKey()
-            roi = cv2.morphologyEx(roi, cv2.MORPH_CLOSE, kernel, iterations=1)
-
-            acc = np.zeros(10)
-            for i in range(10):
-                acc[i] = get_match_score(roi, img_template[i])
-            if np.max(acc) < 0.8:
-                print(acc)
-                digit += '-1'
-            else:
-                digit += str(np.argmax(acc))
-        else:
-            digit = 0
-    try:
-        result = int(digit)
-    except ValueError:
-        result = -1
-    if result > UPPER_NUMMER:
-        return -1, -1
-    else:
-        pass
-        # print(result)
-    return result, result
-
-
-def block_split(img):
-    return img
 
 
 def get_display():
---
+++ Tof1.py
@@ -0,0 +1,86 @@
+import os
+from datetime import datetime
+from gui_server import timer
+import numpy as np
+import serial
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import socket
+import time
+
+from Gui_base import host, port
+
+@timer
+def get_tof(serialport='COM4', data_list=[]):
+    # s = socket.socket()
+    # s.connect((host, int(port)))
+    print(os.path.basename(__file__) + ' bind')
+    print("###starting thread###")
+    with serial.Serial(
+            port=serialport,
+            baudrate=921600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=None) as ser:
+
+        # data storage
+        distarray = np.zeros((4, 4, 8))  # 4 sensors, 4 x 8 pixelmatrix
+        points = np.zeros((4, 4, 8, 3))  # 4 sensors, 4 x 8 pixelmatrix, 3 coordinates (x, y, z)
+
+        # debug_pose = set_debug_pose()
+        count  =0
+        while (1):  # <-- insert read flag here
+            dataraw = bytearray(ser.read_until(b'\xff\xfa\xff\xfa'))
+            data = dataraw[-44:]
+            identifier = data[44 - 7]
+
+            # print('Sensor ID : ',identifier)
+            # status = int.from_bytes(data[44 - 12:44 - 9], 'little')
+            # print('Sensorstatus: ', status)
+
+            if (data[44 - 8] == 1):
+                for i in range(8):
+                    distarray[identifier, 0, i] = int.from_bytes(data[i * 4:i * 4 + 3], 'little')
+                # print(np.array_split(data[:32],8))
+                # distarray2[identifier, 0, 0:7] = int.from_bytes(np.array_split(data[:32],8), 'little')
+                # print(distarray)
+                # print(distarray2)
+                # input()
+                    # print("Reihe 1")
+            elif (data[44 - 8] == 2):
+                for i in range(8):
+                    distarray[identifier, 1, i] = int.from_bytes(data[i * 4:i * 4 + 3], 'little')
+                # print("Reihe 2")
+            elif (data[44 - 8] == 3):
+                for i in range(8):
+                    distarray[identifier, 2, i] = int.from_bytes(data[i * 4:i * 4 + 3], 'little')
+                # print("Reihe 3")
+            elif (data[44 - 8] == 4):
+                for i in range(8):
+                    distarray[identifier, 3, i] = int.from_bytes(data[i * 4:i * 4 + 3], 'little')
+                    # print("Reihe 4")
+            list1 = (['timestamp', datetime.now(), 'identifier', identifier, distarray[identifier, :, :]])
+            data_list.append(list1)
+            if count % 15 ==0 and count!=0:
+                # print(distarray)
+                # print(distarray)
+                a = distarray.tobytes()
+                print(len(a))
+                tof_signal = np.frombuffer(a)
+                b = np.reshape(tof_signal,((4, 4, 8))).copy()
+                b[0][0][0]=1
+                print(distarray)
+                print(b)
+                assert distarray.all()==b.all()
+                input()
+
+
+            count+=1
+        with open("save_list.p", 'wb') as f:
+            f.dump(data_list)
+if __name__ == '__main__':
+    a = np.arange(0,32)
+    print(a)
+    print(np.array_split(a,8))
+    get_tof()
